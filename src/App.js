ff907a73c3fbed54b14295a3aebbfc7f3029936c
kramstyles <mark.eke@decagon.dev>
2022-06-09 21:38:15 +0100

    finalized changes with error when movie not found

--- src/App.js
+++ src/App.js
@@ -6,25 +6,19 @@ import './App.css';
 
 const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}`;
 
-const movie = {
-  "Title": "Amazing Spiderman Syndrome",
-  "Year": "2012",
-  "imdbID": "tt25884554",
-  "Type": "movie",
-  "Poster": "N/A",
-}
-
-
 export const App = () => {
 
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+  const [term, setTerm] = useState('');
 
   const findMovies = async (movieTitle) => {
-    const response = await fetch(`${API_URL}&s=${movieTitle}`);
-    const data = await response.json();
+    if (!movieTitle) alert("Let's be guided please. Enter a movie name!!")
+    else {
+      const response = await fetch(`${API_URL}&s=${movieTitle}`);
+      const data = await response.json();
 
-    setMovies(data.Search);
-    console.log(movies)
+      setMovies(data.Search);
+    }
   }
 
   useEffect(() => {
@@ -37,8 +31,8 @@ export const App = () => {
       <h1>MovieBase</h1>
 
       <div className="search">
-        <input placeholder="Input movie name" value='The Avengers' onChange={() => { }} />
-        <img src={SearchIcon} alt="search Icon" onClick={() => { }} />
+        <input placeholder="The Avengers" value={term} onChange={(e) => setTerm(e.target.value)} />
+        <img src={SearchIcon} alt="search Icon" onClick={() => findMovies(term)} />
       </div>
 
       {
@@ -47,11 +41,11 @@ export const App = () => {
             <MovieCard movie={movie} />
           ))}
         </div> :
-         (
-           <div className='empty'>
-             <h2>No Movies Found!</h2>
-           </div>
-         )
+          (
+            <div className='empty'>
+              <h2>No Movies Found!</h2>
+            </div>
+          )
       }
 
 
