978d409eb64327b34225e6054a7ca3ad20b3b028
kramstyles <mark.eke@decagon.dev>
2022-06-09 17:24:32 +0100

    introduced use effect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const App = () => {
   const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('new counter value', counter)
+  }, [counter]);
 
   return (
     <div>
