b5b6d239a2b5df377aa97a8049a0e5eee225c1aa
kramstyles <mark.eke@decagon.dev>
2022-06-09 20:54:37 +0100

    introduced moviecard.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import {useEffect} from 'react'
 
-// import MovieCard from "./MovieCard";
+import { MovieCard } from "./MovieCard";
 import SearchIcon from "./search.svg";
 import './App.css';
 
 const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}`;
 
-const movie = {
-  "Title" : "Amazing Spiderman Syndrome",
-  "Year" : "2012",
-  "imdbID" : "tt25884554",
-  "Type" : "movie",
-  "Poster" : "N/A",
-}
+
 
 export const App = () => {
 
@@ -37,23 +31,7 @@ export const App = () => {
       </div>
 
       <div className="container">
-        <div className='movie'>
-          <div>
-            <p>{movie.Year}</p>
-          </div>
-
-          <div>
-            <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title} />
-          </div>
-
-          <div>
-            <span>{movie.Type}</span>
-            <h3>{movie.Title}</h3>
-          </div>
-
-        </div>
-
-        
+        <MovieCard />
       </div>
     </div>
   )
